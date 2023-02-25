f92a6d0d03d3ae675c57b29ccb5ceae883f2e3c4
MoToR <alex.bublik.82@gmail.com>
2023-02-25 18:14:02 +0200

    cube_3d

---
+++ cube_3d.py
@@ -0,0 +1,19 @@
+from game_objects import *
+
+app = Ursina()
+window.color = color.black
+
+AmbientLight(type='ambient', color=(0.5, 0.5, 0.5, 1))
+DirectionalLight(type='directional', color=(0.5, 0.5, 0.5, 1), direction=(1, 1, 1))
+cube = Entity(model="cube", rotation=(-15, 0, 0), position=(0, 0, 0), scale=4, color=color.orange)
+
+def update():
+    cube.rotation_y += time.dt * 50
+    cube.x += held_keys['d'] * time.dt * 5
+    cube.x -= held_keys['a'] * time.dt * 5
+
+    cube.rotation_x += time.dt * 50
+    cube.y += held_keys['s'] * time.dt * 5
+    cube.y -= held_keys['w'] * time.dt * 5
+
+app.run()
---
+++ game_objects.py
@@ -0,0 +1,12 @@
+from ursina import *
+from random import randrange
+
+
+class Apple(Entity):
+    def __init__(self, MAP_SIZE, **kwargs):
+        super().__init__(**kwargs)
+        self.MAP_SIZE = MAP_SIZE
+        self.new_position()
+
+    def new_position(self):
+        self.position = (randrange(self.MAP_SIZE) + 0.5, randrange(self.MAP_SIZE) + 0.5, -0.5)
