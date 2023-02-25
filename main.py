d2ca8cab0e716f1cbe29867422bd254e536c817f
alxaoc <alex.bublik.82@gmail.com>
2023-02-25 14:19:18 +0200

    test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Ursina()
 window.color = color.black
 
 Light(type='ambient', color=(0.5, 0.5, 0.5, 1))
-Light(type='directional', color=(0.5, 0.5, 0.5, 1), direction=(1, 1, 1, 1))
+Light(type='directional', color=(0.5, 0.5, 0.5, 1), direction=(1, 1, 1))
 cube = Entity(model="cube", rotatioon=(-15, 0, 0), positio=(0, 0, 0), scale=4, color=color.orange)
 
 app.run()
