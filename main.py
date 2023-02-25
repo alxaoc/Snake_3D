fe0cc50f5fe6fe7ff7eeb06fd58e1b93b31218ad
alxaoc <alex.bublik.82@gmail.com>
2023-02-25 14:20:57 +0200

    rotation

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ window.color = color.black
 
 Light(type='ambient', color=(0.5, 0.5, 0.5, 1))
 Light(type='directional', color=(0.5, 0.5, 0.5, 1), direction=(1, 1, 1))
-cube = Entity(model="cube", rotatioon=(-15, 0, 0), positio=(0, 0, 0), scale=4, color=color.orange)
+cube = Entity(model="cube", rotation=(-15, 0, 0), positio=(0, 0, 0), scale=4, color=color.orange)
 
 app.run()
